db14238b4052db7396e3513f7dec7e2926c88458
sudheer-2323 <sudheermuppineedi23@gmail.com>
2025-05-12 19:34:26 +0530

    modified

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 db.connect((err) => {
