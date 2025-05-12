10e01f772994831cd8aa4177714e1d61d47e548b
sudheer-2323 <sudheermuppineedi23@gmail.com>
2025-05-12 22:14:48 +0530

    final

--- server.js
+++ server.js
@@ -59,5 +59,5 @@ app.get('/listSchools', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
