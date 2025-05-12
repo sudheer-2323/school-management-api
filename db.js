32c0ac8485637717681971caf86fddbfd995eadb
sudheer-2323 <sudheermuppineedi23@gmail.com>
2025-05-12 21:38:35 +0530

    modified

--- db.js
+++ db.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: mysql.railway.internal,
-  user: root,
-  password: dUQoxlFNDfrDapxSbRphCdyYcmfkxyCC,
-  database: railway,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
